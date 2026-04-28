347828b03ff6c3b152f59bd9098be4d4fc103de5
Guilherme Nunes <guilherme.ibnunes@gmail.com>
2026-04-28 20:20:48 -0300

    Adicionando favicon

--- backend/src/config/mongo.ts
+++ backend/src/config/mongo.ts
@@ -6,8 +6,8 @@ export const conectarMongo = async () => {
     const url = 'mongodb://127.0.0.1:27017/acessos_sistema_odonto';
     
     await mongoose.connect(url);
-    console.log('📦 Conectado ao MongoDB com sucesso!');
+    console.log('Conectado ao MongoDB com sucesso!');
   } catch (error) {
-    console.error('❌ Erro ao conectar ao MongoDB:', error);
+    console.error('Erro ao conectar ao MongoDB:', error);
   }
 };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,9 +4,9 @@ import { conectarMongo } from './config/mongo';
 const PORT = 3001; 
 conectarMongo().then(() => {
   app.listen(PORT, () => {
-    console.log(`✅ MongoDB conectado com sucesso!`);
-    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+    console.log(`MongoDB conectado com sucesso!`);
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
   });
 }).catch(err => {
-  console.error("❌ Falha na conexão com o banco:", err);
+  console.error("Falha na conexão com o banco:", err);
 });
